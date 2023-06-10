ef234c5d9a758325eccff54c0aaaf03b8577ce29
kevin <kevindiegodasilvasousa@gmail.com>
2023-06-10 08:53:43 -0300

    chore: update routes

--- linkTreeDC/src/App.jsx
+++ linkTreeDC/src/App.jsx
@@ -1,23 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from "./components/Login";
-import { NotFound } from "./components/NotFound";
-import { Register } from "./components/Register";
-import { ForgotPassword } from "./components/ForgotPassword";
+import { Ways } from "./routes/Ways";
+
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={""}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgotpassword" element={<ForgotPassword />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Ways/>
   );
 }
 
---
+++ linkTreeDC/src/routes/Ways.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "../components/Login";
+import { NotFound } from "../components/NotFound";
+import { Register } from "../components/Register";
+import { ForgotPassword } from "../components/ForgotPassword";
+
+export const Ways = () => {
+    return(
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={""}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    )
+}
